0cf4349317f00071fe9fbe4474aac281327f1f58
Kai-Oliver Prott <kai.prott@hotmail.de>
2021-01-20 12:33:21 +0100

    Remove any test that was CurryPlugin specific

--- test/SemanticTests.hs
+++ test/SemanticTests.hs
@@ -5,63 +5,64 @@ module SemanticTests where
 import Distribution.TestSuite
 import Control.Concurrent
 
-import Plugin.CurryPlugin.Encapsulation
+import Plugin.SMLPlugin.Encapsulation
 
-import qualified Tests.Guards     as Test
-import qualified Tests.UnknownNat as Test
-import qualified Tests.LetPattern as Test
-import qualified Tests.Bang       as Test
+-- import qualified Tests.Guards     as Test
+-- import qualified Tests.UnknownNat as Test
+-- import qualified Tests.LetPattern as Test
+-- import qualified Tests.Bang       as Test
 
 data SemanticTestDescr = forall a. (Eq a, Show a) => TestDescr
-  {  testExpr   :: a
-  ,  testResult :: a
+  {  testExpr   :: IO a
+  ,  testResult :: IO a
   ,  testName   :: String
   }
 
-unknownNat :: SemanticTestDescr
-unknownNat = TestDescr
-  { testExpr   = take 1 (eval DFS Test.unknownNat)
-  , testResult = [1]
-  , testName   = "unknownNat"
-  }
+-- unknownNat :: SemanticTestDescr
+-- unknownNat = TestDescr
+--   { testExpr   = take 1 (eval DFS Test.unknownNat)
+--   , testResult = [1]
+--   , testName   = "unknownNat"
+--   }
 
-guards :: SemanticTestDescr
-guards = TestDescr
-  { testExpr   = take 1 (eval2 DFS Test.take 3 [(1 :: Int)..10])
-  , testResult = [[1,2,3]]
-  , testName   = "take"
-  }
+-- guards :: SemanticTestDescr
+-- guards = TestDescr
+--   { testExpr   = take 1 (eval2 DFS Test.take 3 [(1 :: Int)..10])
+--   , testResult = [[1,2,3]]
+--   , testName   = "take"
+--   }
 
-letPattern :: SemanticTestDescr
-letPattern = TestDescr
-  { testExpr   = not (null (eval DFS Test.letPattern))
-  , testResult = True
-  , testName   = "letPattern"
-  }
+-- letPattern :: SemanticTestDescr
+-- letPattern = TestDescr
+--   { testExpr   = not (null (eval DFS Test.letPattern))
+--   , testResult = True
+--   , testName   = "letPattern"
+--   }
 
-bangPattern :: SemanticTestDescr
-bangPattern = TestDescr
-  { testExpr   = null (eval DFS Test.testBang)
-  , testResult = True
-  , testName   = "bangPattern"
-  }
+-- bangPattern :: SemanticTestDescr
+-- bangPattern = TestDescr
+--   { testExpr   = null (eval DFS Test.testBang)
+--   , testResult = True
+--   , testName   = "bangPattern"
+--   }
 
-noBangPattern :: SemanticTestDescr
-noBangPattern = TestDescr
-  { testExpr   = eval DFS Test.testNoBang
-  , testResult = [()]
-  , testName   = "noBangPattern"
-  }
+-- noBangPattern :: SemanticTestDescr
+-- noBangPattern = TestDescr
+--   { testExpr   = eval DFS Test.testNoBang
+--   , testResult = [()]
+--   , testName   = "noBangPattern"
+--   }
 
 mkSemanticTest :: SemanticTestDescr -> TestInstance
 mkSemanticTest (TestDescr e expected nm) = TestInstance
   { run = do
-      res <- timeout 2000000 (return (e `seq` e))
+      res <- timeout 2000000 (e `seq` e)
+      expec <- expected
       return $ case res of
-        Just r | r == expected
+        Just r | r == expec
                  -> Finished Pass
                | otherwise
-                 -> Finished (Fail ("Expected: " ++ show expected ++
+                 -> Finished (Fail ("Expected: " ++ show expec ++
                                     ", but the result is: " ++ show r))
         Nothing  -> Finished (Fail "Computation timed out")
   , name = nm
