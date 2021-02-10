db5cbb028e47658bfd6d07c2a8f9727449e5347f
Kai-Oliver Prott <kai.prott@hotmail.de>
2021-02-10 17:45:49 +0100

    Add missing Strict flag

--- src/Plugin/SMLPlugin.hs
+++ src/Plugin/SMLPlugin.hs
@@ -1,14 +1,23 @@
 module Plugin.SMLPlugin (plugin) where
 
 import GHC.Plugins
+import Language.Haskell.TH (Extension(..))
 
 import Plugin.LanguagePlugin
 import Plugin.Trans.Config
 
 plugin :: Plugin
-plugin = setConfigFlagsFor flags languagePlugin
+plugin = addStrictFlag (setConfigFlagsFor flags languagePlugin)
   where
     flags = [ (fst monadModConfigStr, "Plugin.Effect.Monad")
             , (fst monadNameConfigStr, "SML")
             , (fst preludeModConfigStr, "Plugin.SMLPlugin.Prelude")
             , (fst builtInModConfigStr, "Plugin.SMLPlugin.BuiltIn") ]
+
+addStrictFlag :: Plugin -> Plugin
+addStrictFlag pl@(Plugin { dynflagsPlugin = dflagsPl }) =
+  pl { dynflagsPlugin = setStrict }
+  where
+    setStrict cmdOpts flags = do
+      pluginFlags <- dflagsPl cmdOpts flags
+      return (pluginFlags `xopt_set` Strict `xopt_set` StrictData)
